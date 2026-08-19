02b865f01107a1ae8dc127c20d9398ac1bcddc7d
Sourav Kumar <xsouravkumar@gmail.com>
2026-08-19 19:37:55 +0530

    server project initialized

---
+++ server/src/main/java/com/asyncsourav/quicktix/QuicktixApplication.java
@@ -0,0 +1,13 @@
+package com.asyncsourav.quicktix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuicktixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuicktixApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/asyncsourav/quicktix/QuicktixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asyncsourav.quicktix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuicktixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
