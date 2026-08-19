ca0deb506f3ae56dc084da790f85e1196e74f187
Sourav Kumar <xsouravkumar@gmail.com>
2026-08-20 03:14:19 +0530

    made a model for user entity

--- server/src/test/java/com/asyncsourav/quicktix/QuicktixApplicationTests.java
+++ server/src/test/java/com/asyncsourav/quicktix/QuicktixApplicationTests.java
@@ -2,8 +2,10 @@ package com.asyncsourav.quicktix;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class QuicktixApplicationTests {
 
 	@Test
