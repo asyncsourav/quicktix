9859f49d2b78e11c2a88636885dbfb81c64fb691
Sourav Kumar <xsouravkumar@gmail.com>
2026-08-22 23:05:10 +0530

    completed the user entity

---
+++ server/src/main/java/com/asyncsourav/quicktix/entity/User.java
@@ -0,0 +1,66 @@
+
+package com.asyncsourav.quicktix.entity;
+
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+
+
+
+@Entity
+@Table(name = "users",
+        indexes = {
+                @Index(name = "idx_users_email", columnList = "email", unique = true)
+        })
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString(exclude = "passwordHash")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name is required field")
+    @Size(max = 100, message = "Name must not exceed 100 characters")
+    @Size(min = 3, message = "Name must be at least 3 characters")
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Must be a valid email address")
+    @Size(max = 150, message = "Email must not exceed 150 characters")
+    @Column(nullable = false, unique = true, length = 150)
+    private String email;
+
+    @NotBlank(message = "password hash is required")
+    @Column(name = "password_hash", nullable = false, length = 255)
+    private String passwordHash;
+
+    @NotNull(message = "Role is required")
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private Role role;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
+
