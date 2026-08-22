c20abc69772b9a78cc9bee49c2af0964fb4c1933
Sourav Kumar <xsouravkumar@gmail.com>
2026-08-23 01:05:52 +0530

    completed the venue entity

---
+++ server/src/main/java/com/asyncsourav/quicktix/entity/Venue.java
@@ -0,0 +1,59 @@
+
+package com.asyncsourav.quicktix.entity;
+
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+
+
+
+@Entity
+@Table(name = "venues")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString(exclude = "organizer")
+public class Venue {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Venue name is required")
+    @Size(max = 150, message = "Venue name must not exceed 150 characters")
+    @Column(nullable = false, length = 150)
+    private String name;
+
+    @NotBlank(message = "Venue address is required")
+    @Size(max = 255, message = "Venue address must not exceed 255 characters")
+    @Column(nullable = false, length = 255)
+    private String address;
+
+    @NotNull(message = "Total capacity is required")
+    @Min(value = 1, message = "Total capacity must be at least 1")
+    @Max(value = 1000, message = "total capacity cannot exceed more than 1000")
+    @Column(name = "total_capacity", nullable = false)
+    private Integer totalCapacity;
+
+    @NotNull(message = "Organizer is required")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "organizer_id", nullable = false)
+    private User organizer;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
