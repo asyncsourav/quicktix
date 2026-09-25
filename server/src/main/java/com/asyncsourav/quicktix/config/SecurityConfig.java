2ff810b024af19b1ce1e231c111aea287874c3db
Sourav Kumar <xsouravkumar@gmail.com>
2026-09-25 19:37:35 +0530

    enabled configuration

--- server/src/main/java/com/asyncsourav/quicktix/config/SecurityConfig.java
+++ server/src/main/java/com/asyncsourav/quicktix/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import com.asyncsourav.quicktix.security.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -19,9 +20,9 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 
 
-
-@Configuration
+@Configuration 
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
